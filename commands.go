0ae813a686d2bd1c5086c66d47401debeedbffca
Nathan Conlon <nconlon@pm.me>
2026-01-31 16:41:25 -0600

    add reset command to clear users table

--- commands.go
+++ commands.go
@@ -91,3 +91,14 @@ func handlerRegister(s *state, cmd command) error {
 	fmt.Printf("User Created: %+v", result)
 	return nil
 }
+
+func handlerReset(s *state, cmd command) error {
+	ctx := context.Background()
+	err := s.Db.DeleteUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to delete: %w", err)
+	}
+
+	fmt.Printf("users reset")
+	return nil
+}
